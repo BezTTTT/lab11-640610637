7a3f8a482b0497ce110901322c4c5cb4127bfcc2
BezTTTT <Best_1614@hotmail.com>
2022-09-29 15:10:06 +0700

    login finish

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -5,17 +5,43 @@ import bcrypt from "bcrypt";
 export default function login(req, res) {
   if (req.method === "POST") {
     const { username, password } = req.body;
-
-    //validate body
-
+    if (
+      typeof username !== "string" ||
+      username.length === 0 ||
+      typeof password !== "string" ||
+      password.length === 0
+    )
+      //validate body
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username or password cannot be empty" });
     const users = readUsersDB();
 
     //find users with username, password
+    const foundUser = users.find(
+      (x) => x.username === username && bcrypt.compareSync(password, x.password)
+    );
+    if (!foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid Username or Password" });
 
     const secret = process.env.JWT_SECRET;
-
+    const token = jwt.sign(
+      {
+        username: foundUser.username,
+        isAdmin: foundUser.isAdmin,
+      },
+      secret,
+      { expiresIn: "1800s" }
+    );
     //sign token
-
+    return res.json({
+      ok: true,
+      username: foundUser.username,
+      isAdmin: foundUser.isAdmin,
+      token,
+    });
     //return response
   }
 }
